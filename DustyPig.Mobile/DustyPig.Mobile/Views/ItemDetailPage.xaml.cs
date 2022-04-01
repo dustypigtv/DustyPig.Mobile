9a4b132c7d947d450f6c97bcf1d3a6ef83b4e6bc
jasondavis303 <jasondavis303@gmail.com>
2022-04-01 12:03:16 -0600

    Add project files.

---
+++ DustyPig.Mobile/DustyPig.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DustyPig.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DustyPig.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
