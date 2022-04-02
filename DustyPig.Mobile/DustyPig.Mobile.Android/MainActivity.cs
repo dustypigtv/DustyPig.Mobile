79913f3140a3f4a9126a25ba0acd07030ac012bd
jasondavis303 <jasondavis303@gmail.com>
2022-04-02 17:18:42 -0600

    Do android splash screen correctly

--- DustyPig.Mobile/DustyPig.Mobile.Android/MainActivity.cs
+++ DustyPig.Mobile/DustyPig.Mobile.Android/MainActivity.cs
@@ -7,11 +7,12 @@ using Android.OS;
 
 namespace DustyPig.Mobile.Droid
 {
-    [Activity(Label = "Dusty Pig", LaunchMode = LaunchMode.SingleTop, Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Dusty Pig", LaunchMode = LaunchMode.SingleTop, Icon = "@mipmap/icon", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            SetTheme(Resource.Style.MainTheme);
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
