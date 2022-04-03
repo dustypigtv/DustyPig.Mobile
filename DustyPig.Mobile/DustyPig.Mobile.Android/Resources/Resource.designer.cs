f2e90bfdecffa66201eecb61625f580c1e751583
jason <jasondavis303@gmail.com>
2022-04-03 08:12:28 -0600

    * DustyPig.Mobile.iOS.csproj: * Resource.designer.cs:
    * Info.plist: Changes to run on vs mac

--- DustyPig.Mobile/DustyPig.Mobile.Android/Resources/Resource.designer.cs
+++ DustyPig.Mobile/DustyPig.Mobile.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace DustyPig.Mobile.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.99.104")]
 	public partial class Resource
 	{
 		
