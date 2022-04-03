1ba6bbf9d9b803e49e45a86e49c05597cb934ff4
JD <dev@dustypig.tv>
2022-04-03 03:05:27 -0400

    Set android app icon

--- DustyPig.Mobile/DustyPig.Mobile.Android/Resources/Resource.designer.cs
+++ DustyPig.Mobile/DustyPig.Mobile.Android/Resources/Resource.designer.cs
@@ -18792,9 +18792,6 @@ namespace DustyPig.Mobile.Droid
 			// aapt resource value: 0x7F0C0001
 			public const int icon_round = 2131492865;
 			
-			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
-			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
