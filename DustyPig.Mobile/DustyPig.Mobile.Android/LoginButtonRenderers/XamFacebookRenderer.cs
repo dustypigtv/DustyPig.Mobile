f94641bb403566c7ef9fd661ac656f11e6f7ea97
JD <dev@dustypig.tv>
2022-04-08 12:41:36 -0400

    Fix Facebook button layout

--- DustyPig.Mobile/DustyPig.Mobile.Android/LoginButtonRenderers/XamFacebookRenderer.cs
+++ DustyPig.Mobile/DustyPig.Mobile.Android/LoginButtonRenderers/XamFacebookRenderer.cs
@@ -2,6 +2,7 @@
 using Android.Util;
 using DustyPig.Mobile.Droid.LoginButtonRenderers;
 using DustyPig.Mobile.LoginButtons;
+using System.ComponentModel;
 using Xamarin.Facebook;
 using Xamarin.Facebook.Login;
 using Xamarin.Facebook.Login.Widget;
@@ -12,16 +13,19 @@ using Xamarin.Forms.Platform.Android;
 [assembly: ExportRenderer(typeof(XamFacebook), typeof(XamFacebookRenderer))]
 namespace DustyPig.Mobile.Droid.LoginButtonRenderers
 {
-    public class XamFacebookRenderer : ViewRenderer, IFacebookCallback
+    public class XamFacebookRenderer : ViewRenderer<XamFacebook, LoginButton>, IFacebookCallback
     {
         private bool _disposed = false;
 
         public XamFacebookRenderer(Context context) : base(context)
         {
-
+            
         }
 
-        private XamFacebook XamElement => (XamFacebook)Element;
+        private LoginButton NativeButton => (LoginButton)Control;
+
+        private XamFacebook XamButton => (XamFacebook)Element;
+        
 
         protected override void Dispose(bool disposing)
         {
@@ -39,35 +43,34 @@ namespace DustyPig.Mobile.Droid.LoginButtonRenderers
 
 
 
-        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+        protected override void OnElementChanged(ElementChangedEventArgs<XamFacebook> e)
         {
             if (Control == null)
             {
                 //When this button is shown, it means nobody is logged in. Make sure that facebook is in the same state
                 LoginManager.Instance.LogOut();
 
-                var fbButton = new LoginButton(Context) { LoginBehavior = LoginBehavior.NativeWithFallback };                
+                var fbButton = new LoginButton(Context) { LoginBehavior = LoginBehavior.NativeWithFallback };
+
+                fbButton.SetTextSize(ComplexUnitType.Dip, e.NewElement.TextSize);
+
+                int paddingTop = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, e.NewElement.PaddingTop, Context.Resources.DisplayMetrics);
+                int paddingBottom = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, e.NewElement.PaddingBottom, Context.Resources.DisplayMetrics);
+                fbButton.SetPadding(fbButton.PaddingLeft, paddingTop, fbButton.PaddingRight, paddingBottom);
+                
                 fbButton.SetPermissions(new string[] { "public_profile", "email" });
                 fbButton.RegisterCallback(MainActivity.CallbackManager, this);
-
-                // *** TO DO ***
-                //This is hacky, there must be a better way...
-                //Based on button height of 40
-                fbButton.SetTextSize(ComplexUnitType.Dip, 15);
-                int padding = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, 10, Context.Resources.DisplayMetrics);
-                fbButton.SetPadding(fbButton.PaddingLeft, padding, fbButton.PaddingRight, padding);
 
                 SetNativeControl(fbButton);
             }
         }
 
 
+        public void OnCancel() => XamButton.OnCancel?.Execute(null);
 
-        public void OnCancel() => XamElement.OnCancel?.Execute(null);
+        public void OnError(FacebookException fbException) => XamButton.OnError?.Execute(fbException.Message);
 
-        public void OnError(FacebookException fbException) => XamElement.OnError?.Execute(fbException.Message);
-
-        public void OnSuccess(Java.Lang.Object result) => XamElement.OnSuccess?.Execute(((LoginResult)result).AccessToken.Token);
+        public void OnSuccess(Java.Lang.Object result) => XamButton.OnSuccess?.Execute(((LoginResult)result).AccessToken.Token);
 
     }
 }
--- DustyPig.Mobile/DustyPig.Mobile/LoginButtons/XamFacebook.cs
+++ DustyPig.Mobile/DustyPig.Mobile/LoginButtons/XamFacebook.cs
@@ -1,37 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Xamarin.Forms;
 
 namespace DustyPig.Mobile.LoginButtons
 {
     public class XamFacebook : View
     {
+        public static readonly BindableProperty OnSuccessProperty = BindableProperty.Create(nameof(OnSuccess), typeof(Command<string>), typeof(XamFacebook));
+
         public Command<string> OnSuccess
         {
-            get { return (Command<string>)GetValue(OnSuccessProperty); }
-            set { SetValue(OnSuccessProperty, value); }
+            get => (Command<string>)GetValue(OnSuccessProperty);
+            set => SetValue(OnSuccessProperty, value);
         }
 
-        public static readonly BindableProperty OnSuccessProperty =
-            BindableProperty.Create(nameof(OnSuccess), typeof(Command<string>), typeof(XamFacebook));
+
+
+        public static readonly BindableProperty OnErrorProperty = BindableProperty.Create(nameof(OnError), typeof(Command<string>), typeof(XamFacebook));
 
         public Command<string> OnError
         {
-            get { return (Command<string>)GetValue(OnErrorProperty); }
-            set { SetValue(OnErrorProperty, value); }
+            get => (Command<string>)GetValue(OnErrorProperty);
+            set => SetValue(OnErrorProperty, value);
         }
 
-        public static readonly BindableProperty OnErrorProperty =
-            BindableProperty.Create(nameof(OnError), typeof(Command<string>), typeof(XamFacebook));
 
+
+        public static readonly BindableProperty OnCancelProperty = BindableProperty.Create(nameof(OnCancel), typeof(Command), typeof(XamFacebook));
+        
         public Command OnCancel
         {
-            get { return (Command)GetValue(OnCancelProperty); }
-            set { SetValue(OnCancelProperty, value); }
+            get => (Command)GetValue(OnCancelProperty);
+            set => SetValue(OnCancelProperty, value);
         }
 
-        public static readonly BindableProperty OnCancelProperty =
-            BindableProperty.Create(nameof(OnCancel), typeof(Command), typeof(XamFacebook));
+
+
+        public static readonly BindableProperty PaddingTopProperty = BindableProperty.Create(nameof(PaddingTop), typeof(float), typeof(XamFacebook), 5.091f, BindingMode.OneWay);
+
+        public float PaddingTop
+        {
+            get => (float)GetValue(PaddingTopProperty);
+            set => SetValue(PaddingTopProperty, value);
+        }
+
+
+
+        public static readonly BindableProperty PaddingBottomProperty = BindableProperty.Create(nameof(PaddingBottom), typeof(float), typeof(XamFacebook), 5.091f, BindingMode.OneWay);
+
+        public float PaddingBottom
+        {
+            get => (float)GetValue(PaddingBottomProperty);
+            set => SetValue(PaddingBottomProperty, value);
+        }
+
+
+
+        public static readonly BindableProperty TextSizeProperty = BindableProperty.Create(nameof(TextSize), typeof(float), typeof(XamFacebook), 14.02f, BindingMode.OneWay);
+
+        public float TextSize
+        {
+            get => (float)GetValue(TextSizeProperty);
+            set => SetValue(TextSizeProperty, value);
+        }
     }
 }
