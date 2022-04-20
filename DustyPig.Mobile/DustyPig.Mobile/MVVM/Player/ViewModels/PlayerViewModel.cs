83101fa49f0f29cf3c1939c2e320cd61dc586be4
JD <dev@dustypig.tv>
2022-04-19 23:27:02 -0600

    Setup more empty pages

---
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/Player/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DustyPig.Mobile.MVVM.Player.ViewModels
+{
+    internal class PlayerViewModel
+    {
+    }
+}
---
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/Settings/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DustyPig.Mobile.MVVM.Settings.ViewModels
+{
+    internal class SettingsViewModel
+    {
+    }
+}
