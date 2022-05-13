d245182994762eb7b501c66a4b7ffd59dd4eae11
JD <dev@dustypig.tv>
2022-05-12 19:09:47 -0600

    Work on MovieDetails page

--- DustyPig.Mobile/DustyPig.Mobile/MVVM/MediaDetails/Movie/MovieDetailsViewModel.cs
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/MediaDetails/Movie/MovieDetailsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using DustyPig.API.v3.Models;
+using System;
 
 namespace DustyPig.Mobile.MVVM.MediaDetails.Movie
 {
@@ -12,14 +13,52 @@ namespace DustyPig.Mobile.MVVM.MediaDetails.Movie
 
         public BasicMedia Basic { get; }
 
+        private string _year;
+        public string Year
+        {
+            get => _year;
+            set => SetProperty(ref _year, value);
+        }
+
+        
+        
         public async void OnAppearing()
         {
+            IsBusy = true;
+
             var response = await App.API.Movies.GetDetailsAsync(Basic.Id);
             if (response.Success)
             {
                 BackdropUrl = string.IsNullOrWhiteSpace(response.Data.BackdropUrl) ?
                     response.Data.ArtworkUrl :
                     response.Data.BackdropUrl;
+                Title = response.Data.Title;
+                Year = response.Data.Date.Year.ToString();
+                Rating = response.Data.Rated.ToString().Replace('_', '-');
+                Description = response.Data.Description;
+                Played = response.Data.Played ?? 0;
+                ShowPlayedBar = Played > 0;
+                CastString = string.Join(", ", response.Data.Cast);
+                DirectorsString = string.Join(", ", response.Data.Directors);
+                Duration = response.Data.Length;
+
+
+                Progress = Math.Min(Math.Max(Played / Duration, 0), 1);
+                PlayButtonText = Played > 0 ? "Resume" : "Play";
+               
+                var dur = TimeSpan.FromSeconds(response.Data.Length);
+                if (dur.Hours > 0)
+                    DurationString = $"{dur.Hours}h {dur.Minutes}m";
+                else
+                    DurationString = $"{Math.Max(dur.Minutes, 1)}m";
+
+
+                dur = TimeSpan.FromMinutes(response.Data.Length - (response.Data.Played ?? 0));
+                if (dur.Hours > 0)
+                    RemainingString = $"{dur.Hours}h {dur.Minutes}m remaining";
+                else
+                    RemainingString = $"{Math.Max(dur.Minutes, 1)}m remaining";
+
 
             }
             else
@@ -27,6 +66,8 @@ namespace DustyPig.Mobile.MVVM.MediaDetails.Movie
                 await ShowAlertAsync("Error", "Unable to retrieve movie info");
                 await Navigation.PopAsync();
             }
+
+            IsBusy = false;
         }
     }
 }
--- DustyPig.Mobile/DustyPig.Mobile/MVVM/MediaDetails/_DetailsBaseViewModel.cs
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/MediaDetails/_DetailsBaseViewModel.cs
@@ -30,6 +30,83 @@ namespace DustyPig.Mobile.MVVM.MediaDetails
             set => SetProperty(ref _backdropUrl, value);
         }
 
+        private string _rating;
+        public string Rating
+        {
+            get => _rating;
+            set => SetProperty(ref _rating, value);
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
+        private double _duration;
+        public double Duration
+        {
+            get => _duration;
+            set => SetProperty(ref _duration, value);
+        }
+
+        private string _durationString;
+        public string DurationString
+        {
+            get => _durationString;
+            set => SetProperty(ref _durationString, value);
+        }
+
+        private double _progress;
+        public double Progress
+        {
+            get => _progress;
+            set => SetProperty(ref _progress, value);
+        }
+
+        private string _remainingString;
+        public string RemainingString
+        {
+            get => _remainingString;
+            set => SetProperty(ref _remainingString, value);
+        }
+
+        private double _played;
+        public double Played
+        {
+            get => _played;
+            set => SetProperty(ref _played, value);
+        }
+
+        private string _castString;
+        public string CastString
+        {
+            get => _castString;
+            set => SetProperty(ref _castString, value);
+        }
+
+        private string _directorsString;
+        public string DirectorsString
+        {
+            get => _directorsString;
+            set => SetProperty(ref _directorsString, value);
+        }
+
+        private bool _showPlayedBar = false;
+        public bool ShowPlayedBar
+        {
+            get => _showPlayedBar;
+            set => SetProperty(ref _showPlayedBar, value);
+        }
+
+        private string _playButtonText;
+        public string PlayButtonText
+        {
+            get => _playButtonText;
+            set => SetProperty(ref _playButtonText, value);
+        }
+
         public void OnSizeAllocated(double width, double height)
         {
             if (Device.Idiom == TargetIdiom.Phone)
@@ -46,15 +123,16 @@ namespace DustyPig.Mobile.MVVM.MediaDetails
                         break;
 
                     case DisplayOrientation.Portrait:
-                        newWidth = height * 0.5;
+                        newWidth = width * 0.8;
                         break;
                 }
                 if (newWidth < 400)
                     newWidth = width;
-                Width = newWidth;
+
+                Width = (int)Math.Min(width, newWidth);
             }
        
-            ImageHeight = width * 0.5;
+            ImageHeight = (int)(Width * 0.5625);
         }
     }
 }
