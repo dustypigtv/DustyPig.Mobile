f18d79834668b6dc50283ffc837d6894793e3222
JD <dev@dustypig.tv>
2022-05-15 14:22:36 -0600

    Disable tab swipe on android

--- DustyPig.Mobile/DustyPig.Mobile/MVVM/Main/MainPage.xaml.cs
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/Main/MainPage.xaml.cs
@@ -1,15 +1,18 @@
 ﻿
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace DustyPig.Mobile.MVVM.Main
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : Xamarin.Forms.TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().SetIsSwipePagingEnabled(false);
+
         }
     }
 }
