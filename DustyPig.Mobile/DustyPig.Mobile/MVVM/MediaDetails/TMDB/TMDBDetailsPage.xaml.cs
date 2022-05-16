62d10bc91444f11270a3b545fef50a4efb3be5af
JD <dev@dustypig.tv>
2022-05-16 09:15:36 -0600

    TMDB Details

---
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/MediaDetails/TMDB/TMDBDetailsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DustyPig.API.v3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DustyPig.Mobile.MVVM.MediaDetails.TMDB
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TMDBDetailsPage : ContentPage
+    {
+        public TMDBDetailsPage(BasicTMDB basicTMDB)
+        {
+            InitializeComponent();
+            BindingContext = VM = new TMDBDetailsViewModel(basicTMDB, Navigation);
+        }
+
+        public TMDBDetailsViewModel VM { get; }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            VM.OnSizeAllocated(width, height);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            VM.OnAppearing();
+        }
+    }
+}
---
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/MediaDetails/TMDB/TMDBDetailsViewModel.cs
@@ -0,0 +1,119 @@
+﻿using DustyPig.API.v3.Models;
+using DustyPig.Mobile.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace DustyPig.Mobile.MVVM.MediaDetails.TMDB
+{
+    public class TMDBDetailsViewModel : _DetailsBaseViewModel
+    {
+        private BasicTMDB _basicTMDB;
+
+        public TMDBDetailsViewModel(BasicTMDB basicTMDB, INavigation navigation) : base(basicTMDB, navigation)
+        {
+            Id = basicTMDB.TMDB_ID;
+            _basicTMDB = basicTMDB;
+        }
+
+        private ObservableBasicMediaCollection _available = new ObservableBasicMediaCollection();
+        public ObservableBasicMediaCollection Available
+        {
+            get => _available;
+            set => SetProperty(ref _available, value);
+        }
+
+        private bool _showAvailable;
+        public bool ShowAvailable
+        {
+            get => _showAvailable;
+            set => SetProperty(ref _showAvailable, value);
+        }
+
+        private bool _showYear;
+        public bool ShowYear
+        {
+            get => _showYear;
+            set => SetProperty(ref _showYear, value);
+        }
+
+        public async void OnAppearing()
+        {
+            var response = _basicTMDB.MediaType == MediaTypes.Movie
+                ? await App.API.TMDB.GetMovieAsync(Id)
+                : await App.API.TMDB.GetSeriesAsync(Id);
+
+            if (response.Success)
+            {
+                BackdropUrl = string.IsNullOrWhiteSpace(response.Data.BackdropUrl) ?
+                    response.Data.ArtworkUrl :
+                    response.Data.BackdropUrl;
+                Title = response.Data.Title;
+                Year = response.Data.Year.ToString();
+                ShowYear = response.Data.Year > 0;
+                Description = response.Data.Description;
+
+                switch (API.v3.MPAA.RatingsUtils.ToRatings(response.Data.Rated))
+                {
+                    case API.v3.MPAA.Ratings.None:
+                    case API.v3.MPAA.Ratings.NR:
+                        Rating = "Not Rated";
+                        break;
+
+                    default:
+                        Rating = response.Data.Rated.ToString().Replace('_', '-');
+                        break;
+                }
+
+
+                var genres = response.Data.Genres.ToString();
+                if (!string.IsNullOrWhiteSpace(genres))
+                {
+                    Genres = genres.Replace(",", ", ");
+                    ShowGenres = true;
+                }
+
+                if (response.Data.Cast != null && response.Data.Cast.Count > 0)
+                {
+                    Cast = string.Join(", ", response.Data.Cast);
+                    ShowCast = true;
+                }
+
+                if (response.Data.Directors != null && response.Data.Directors.Count > 0)
+                {
+                    Directors = string.Join(", ", response.Data.Directors);
+                    ShowDirectors = true;
+                }
+
+                if (response.Data.Producers != null && response.Data.Producers.Count > 0)
+                {
+                    Producers = string.Join(", ", response.Data.Producers);
+                    ShowProducers = true;
+                }
+
+                if (response.Data.Writers != null && response.Data.Writers.Count > 0)
+                {
+                    Writers = string.Join(", ", response.Data.Writers);
+                    ShowWriters = true;
+                }
+
+                if(response.Data.Available != null && response.Data.Available.Count > 0)
+                {
+                    Available.ReplaceRange(response.Data.Available);
+                    ShowAvailable = true;
+                }
+
+            }
+            else
+            {
+                await ShowAlertAsync("Error", response.Error.Message);
+                await Navigation.PopModalAsync();
+            }
+        }
+
+
+    }
+}
