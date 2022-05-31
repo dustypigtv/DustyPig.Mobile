e4b54279fe45ac2c76dc1758fefef912fa9a5a69
JD <dev@dustypig.tv>
2022-05-31 09:52:47 -0600

    Cach home screen

--- DustyPig.Mobile/DustyPig.Mobile/MVVM/Main/Home/HomeViewModel.cs
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/Main/Home/HomeViewModel.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Collections.Generic;
 using System;
 using DustyPig.API.v3.Models;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace DustyPig.Mobile.MVVM.Main.Home
 {
@@ -181,52 +183,24 @@ namespace DustyPig.Mobile.MVVM.Main.Home
 
 
         private async Task Update()
-        {            
+        {
+            string cache_file = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "home_screen_cache.json");
+
+            bool loadFromDisk = MainStack.Children.Count == 0;
+            if(!loadFromDisk)
+                if (MainStack.Children.Count == 1)
+                    if (MainStack.Children[0] is Label)
+                        loadFromDisk = true;
+
+            if (loadFromDisk)
+                try { AddSections(JsonConvert.DeserializeObject<HomeScreen>(File.ReadAllText(cache_file))); }
+                catch { }
+            
             var response = await App.API.Media.GetHomeScreenAsync();
             if (response.Success)
             {
-                if (response.Data.Sections.Count == 0)
-                {
-                    EmptyLabel.Text = "No media found";
-                    if (!MainStack.Children.Contains(EmptyLabel))
-                    {
-                        MainStack.Children.Clear();
-                        MainStack.Children.Add(EmptyLabel);
-                    }
-                }
-                else
-                {
-                    if (MainStack.Children.Contains(EmptyLabel))
-                        MainStack.Children.Clear();
-
-                    //Remove any from MainStack that isn't in Sections
-                    List<HomePageSectionView> toRemove = new List<HomePageSectionView>();
-                    foreach (HomePageSectionView v in MainStack.Children)
-                        if (!response.Data.Sections.Select(item => item.ListId).Contains(v.VM.ListId))
-                            toRemove.Add(v);
-                    toRemove.ForEach(v => MainStack.Children.Remove(v));
-
-                    //Add new and move around existing
-                    for(int i = 0; i < response.Data.Sections.Count; i++)
-                    {
-                        HomePageSectionView existing = MainStack.Children.FirstOrDefault(item => ((HomePageSectionView)item).VM.ListId == response.Data.Sections[i].ListId) as HomePageSectionView;
-                        if (existing == null)
-                        {
-                            MainStack.Children.Insert(i, new HomePageSectionView(response.Data.Sections[i]));
-                        }
-                        else
-                        {
-                            if (MainStack.Children.IndexOf(existing) != i)
-                            {
-                                MainStack.Children.Remove(existing);
-                                MainStack.Children.Insert(i, existing);                                
-                            }
-                            existing.VM.Items.ReplaceRange(response.Data.Sections[i].Items);
-                            
-                        }
-                    }
-                }
-                               
+                AddSections(response.Data);
+                File.WriteAllText(cache_file, JsonConvert.SerializeObject(response.Data));
                 App.HomePageNeedsRefresh = false;
             }
             else
@@ -237,6 +211,51 @@ namespace DustyPig.Mobile.MVVM.Main.Home
             EmptyView = "No media found";
             App.HomePageNeedsRefresh = false;
             IsBusy = false;
+        }
+   
+        private void AddSections(HomeScreen hs)
+        {
+            if (hs.Sections.Count == 0)
+            {
+                EmptyLabel.Text = "No media found";
+                if (!MainStack.Children.Contains(EmptyLabel))
+                {
+                    MainStack.Children.Clear();
+                    MainStack.Children.Add(EmptyLabel);
+                }
+            }
+            else
+            {
+                if (MainStack.Children.Contains(EmptyLabel))
+                    MainStack.Children.Clear();
+
+                //Remove any from MainStack that isn't in Sections
+                List<HomePageSectionView> toRemove = new List<HomePageSectionView>();
+                foreach (HomePageSectionView v in MainStack.Children)
+                    if (!hs.Sections.Select(item => item.ListId).Contains(v.VM.ListId))
+                        toRemove.Add(v);
+                toRemove.ForEach(v => MainStack.Children.Remove(v));
+
+                //Add new and move around existing
+                for (int i = 0; i < hs.Sections.Count; i++)
+                {
+                    HomePageSectionView existing = MainStack.Children.FirstOrDefault(item => ((HomePageSectionView)item).VM.ListId == hs.Sections[i].ListId) as HomePageSectionView;
+                    if (existing == null)
+                    {
+                        MainStack.Children.Insert(i, new HomePageSectionView(hs.Sections[i]));
+                    }
+                    else
+                    {
+                        if (MainStack.Children.IndexOf(existing) != i)
+                        {
+                            MainStack.Children.Remove(existing);
+                            MainStack.Children.Insert(i, existing);
+                        }
+                        existing.VM.Items.ReplaceRange(hs.Sections[i].Items);
+
+                    }
+                }
+            }
         }
     }
 }
