5929910052540e77eeef7cad04ace77a5c4b1442
JD <dev@dustypig.tv>
2022-06-01 00:49:45 -0600

    Handle back button on main tabbed page

--- DustyPig.Mobile/DustyPig.Mobile/MVVM/Main/MainPage.xaml.cs
+++ DustyPig.Mobile/DustyPig.Mobile/MVVM/Main/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
@@ -9,6 +10,8 @@ namespace DustyPig.Mobile.MVVM.Main
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : Xamarin.Forms.TabbedPage
     {
+        readonly Stack<int> _tabHistory = new Stack<int>();
+
         public MainPage()
         {
             InitializeComponent();
@@ -20,12 +23,27 @@ namespace DustyPig.Mobile.MVVM.Main
         {
             base.OnCurrentPageChanged();
 
+            _tabHistory.Push(Children.IndexOf(CurrentPage));
+
             var cp = CurrentPage as NavigationPage;
             if (cp == null)
                 return;
 
             if (cp.RootPage is Search.SearchPage sp)
                 sp.InvokePageShown();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            //return base.OnBackButtonPressed();
+
+            if (_tabHistory.Count > 1)
+            {
+                _tabHistory.Pop();
+                CurrentPage = Children[_tabHistory.Pop()];
+            }
+
+            return true;
         }
 
     }
